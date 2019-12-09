6c624fc82de89b045f23d792764b301c57d531cf
Aleksandr <markovkin88@gmail.com>
2019-12-09 23:11:11 +0300

    Убирает лишние строки комментарие в main.js

--- src/main.js
+++ src/main.js
@@ -1,16 +1,10 @@
 import MainMenuComponent from './components/mainMenu.js';
 import UserRankComponent from './components/userRank.js';
-
 import PageControllerComponent from './controllers/pageController.js';
 import {generateDatum} from './mock/datum.js';
 import {render, RenderPosition} from './utils/render.js';
 import {CARD_COUNT} from './const.js';
 
-// const addCloseEventListenerPopup = (popup) => {
-//   popup.getElement().querySelector(`.film-details__close-btn`).addEventListener(`click`, () => {
-//     closePopup(popup);
-//   });
-// };
 const CARD_IN_FILMS_COUNT = 5;
 
 const headerElement = document.querySelector(`.header`);
@@ -18,15 +12,6 @@ const mainElement = document.querySelector(`.main`);
 
 render(mainElement, new MainMenuComponent(CARD_IN_FILMS_COUNT).getElement(), RenderPosition.BEFOREEND);
 render(headerElement, new UserRankComponent().getElement(), RenderPosition.BEFOREEND);
-
-
-// render(filmsElement, new FilmsListComponent(), RenderPosition.BEFOREEND);
-// render(filmsElement, new FilmsListComponent(FILMS_LIST_EXTRA_MARKUP, TOP_RATED_MARKUP), RenderPosition.BEFOREEND);
-// render(filmsElement, new FilmsListComponent(FILMS_LIST_EXTRA_MARKUP, MOST_COMMENTED_MARKUP), RenderPosition.BEFOREEND);
-
-// const filmListContainerElement = mainElement.querySelector(`.films-list__container`);
-// const filmTopRatedElement = filmsElement.querySelector(`.films-list--extra .films-list__container`);
-// const filmMostCommentedElement = filmsElement.querySelector(`.films-list--extra:last-child .films-list__container`);
 
 const datum = generateDatum(CARD_COUNT);
 
