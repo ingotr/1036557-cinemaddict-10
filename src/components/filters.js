5d1a9ccafca89418da86b4d80097670099bb7998
Aleksandr <markovkin88@gmail.com>
2019-12-09 18:46:12 +0300

    Добавляет виды сортировки

--- src/components/filters.js
+++ src/components/filters.js
@@ -1,14 +1,26 @@
 import AbstractComponent from './abstractComponent.js';
 
+export const SortType = {
+  DATE_DOWN: `date-down`,
+  RATING_DOWN: `rating-down`,
+  DEFAULT: `default`,
+};
+
 const createFiltersTemplate = () => (
   `<ul class="sort">
-  <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
-  <li><a href="#" class="sort__button">Sort by date</a></li>
-  <li><a href="#" class="sort__button">Sort by rating</a></li>
+    <li><a href="#" data-sort-type="${SortType.DEFAULT}" class="sort__button sort__button--active">Sort by default</a></li>
+    <li><a href="#" data-sort-type="${SortType.DATE_DOWN}" class="sort__button">Sort by date</a></li>
+    <li><a href="#" data-sort-type="${SortType.RATING_DOWN}" class="sort__button">Sort by rating</a></li>
   </ul>`
 );
 
 export default class Filters extends AbstractComponent {
+  constructor() {
+    super();
+
+    this._currentSortType = SortType.DEFAULT;
+  }
+
   getTemplate() {
     return createFiltersTemplate();
   }
