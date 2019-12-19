3d45ecf985657f8a985837ac6be1fc94fc5aa267
Aleksandr <markovkin88@gmail.com>
2019-12-19 21:33:01 +0300

    webpack.config: Обновляет конфигурацию для обновления webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,17 @@ const path = require(`path`);
 
 module.exports = {
   mode: `development`,
-  entry: [`./src/main.js`],
+  entry: `./src/main.js`,
   output: {
+    filename: `bundle.js`,
     // eslint-disable-next-line no-undef
     path: path.join(__dirname, `public`),
-    filename: `bundle.js`,
   },
   devtool: `source-map`,
   devServer: {
     // eslint-disable-next-line no-undef
     contentBase: path.join(__dirname, `public`),
-    publicPath: `http:/localhost:8080/`,
     compress: true,
-    open: true,
     watchContentBase: true,
   },
 };
