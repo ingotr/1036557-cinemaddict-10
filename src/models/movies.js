226cd500849f1ef00518721b1596fb5dced20d80
Markovkin Aleksandr <markovkin88@gmail.com>
2019-12-24 13:32:25 +0300

    models/movies: Добавляет метод обновления карточки фильма

--- src/models/movies.js
+++ src/models/movies.js
@@ -10,4 +10,17 @@ export default class Movies {
   setMovies(movies) {
     this._movies = Array.from(movies);
   }
+
+  updateMovie(id, movie) {
+    const index = this._movies.findIndex((it) => it.id === id);
+
+    if (index === -1) {
+      return false;
+    }
+
+    this._movies = [].concat(this._movies.slice(0, index), movie,
+        this._movies.slice(index + 1));
+
+    return true;
+  }
 }
