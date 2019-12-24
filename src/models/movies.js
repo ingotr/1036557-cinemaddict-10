a819e8148985422001cdf8923b3ad082d2f08904
Markovkin Aleksandr <markovkin88@gmail.com>
2019-12-24 12:56:08 +0300

    models/Movies: Добавляет методы получения/записи фильмов

--- src/models/movies.js
+++ src/models/movies.js
@@ -2,4 +2,12 @@ export default class Movies {
   constructor() {
     this._movies = [];
   }
+
+  getMovies() {
+    return this._movies;
+  }
+
+  setMovies(movies) {
+    this._movies = Array.from(movies);
+  }
 }
