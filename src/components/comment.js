1a052bd703ce97d22fd86e4f2d8c1229b680cdcb
Aleksandr <markovkin88@gmail.com>
2020-01-11 00:36:27 +0300

    components/comment: Добавляет метод доступа к данным комментария

--- src/components/comment.js
+++ src/components/comment.js
@@ -1,16 +1,17 @@
 import AbstractComponent from './abstract-component.js';
 
 const createCommentMarkup = (comment) => {
-  const {text, emoji, autor, date} = comment;
+  const {text, emoji, author, date} = comment;
+
   return (
     `<li class="film-details__comment">
       <span class="film-details__comment-emoji">
-        <img src="./images/emoji/${emoji}.png" width="55" height="55" alt="emoji">
+        <img src="./${emoji}" width="55" height="55" alt="emoji">
       </span>
       <div>
         <p class="film-details__comment-text">${text}</p>
         <p class="film-details__comment-info">
-          <span class="film-details__comment-author">${autor}</span>
+          <span class="film-details__comment-author">${author}</span>
           <span class="film-details__comment-day">${date}</span>
           <button class="film-details__comment-delete">Delete</button>
         </p>
@@ -28,6 +29,10 @@ export default class Comment extends AbstractComponent {
 
   getTemplate() {
     return createCommentMarkup(this._comment);
+  }
+
+  getCommentData() {
+    return this._comment;
   }
 
   setCommentsDeleteButtonClickHandler(handler) {
