da8396d07c179b010029103449b18d1f2e1a2a4b
Markovkin Aleksandr <markovkin88@gmail.com>
2020-01-20 13:28:32 +0300

    api: Добавляет вспомогательную функцию проверки статуса ответа

--- src/api.js
+++ src/api.js
@@ -10,6 +10,14 @@ const COMMENTS_METHOD = {
   DELETE: `DELETE`
 };
 
+const checkStatus = (response) => {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  } else {
+    throw new Error(`${response.status}: ${response.statusText}`);
+  }
+};
+
 const API = class {
 };
 
