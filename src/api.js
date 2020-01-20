813bfce7e8bd33dcfdce710eed7acb94902a8192
Markovkin Aleksandr <markovkin88@gmail.com>
2020-01-20 13:25:23 +0300

    api: Добавляет перечисление методов http для фильма и комментария

--- src/api.js
+++ src/api.js
@@ -1,3 +1,15 @@
+const MOVIES_METHOD = {
+  GET: `GET`,
+  SYNC: `POST`,
+  PUT: `PUT`,
+};
+
+const COMMENTS_METHOD = {
+  GET: `GET`,
+  POST: `POST`,
+  DELETE: `DELETE`
+};
+
 const API = class {
 };
 
