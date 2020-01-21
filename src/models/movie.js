43474ace2d77764d26f99f86dcb0d97fa5e2e6c0
Aleksandr <markovkin88@gmail.com>
2020-01-22 00:56:58 +0300

    models/movie: Дополняет модель и метод toRaw

--- src/models/movie.js
+++ src/models/movie.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 import {getYearFromIso} from '../utils/common.js';
 export default class Movie {
   constructor(data) {
@@ -7,10 +8,10 @@ export default class Movie {
 
     this.filmInfo = data[`film_info`];
     this.filmInfo.title = this.filmInfo[`title`];
-    this.filmInfo.alternativeTitle = this.filmInfo[`alternative_title`];
-    this.filmInfo.totalRating = this.filmInfo[`total_rating`];
+    this.filmInfo.alternative_title = this.filmInfo[`alternative_title`];
+    this.filmInfo.total_rating = this.filmInfo[`total_rating`];
     this.filmInfo.poster = this.filmInfo[`poster`];
-    this.filmInfo.ageRating = this.filmInfo[`age_rating`];
+    this.filmInfo.age_rating = this.filmInfo[`age_rating`];
     this.filmInfo.director = this.filmInfo[`director`];
     this.filmInfo.actors = this.filmInfo[`actors`];
     this.filmInfo.writers = this.filmInfo[`writers`];
@@ -18,16 +19,16 @@ export default class Movie {
     this.filmInfo.release = this.filmInfo[`release`];
     this.filmInfo.release.year = getYearFromIso(this.filmInfo.release[`date`]);
     this.filmInfo.release.date = this.filmInfo.release[`date`];
-    this.filmInfo.release.releaseCountry = this.filmInfo.release[`release_country`];
+    this.filmInfo.release.release_country = this.filmInfo.release[`release_country`];
     this.filmInfo.runtime = this.filmInfo[`runtime`];
     this.filmInfo.genre = this.filmInfo[`genre`];
     this.filmInfo.description = this.filmInfo[`description`];
 
     this.userDetails = data[`user_details`];
-    this.userDetails.personalRating = this.userDetails[`personal_rating`];
+    this.userDetails.personal_rating = this.userDetails[`personal_rating`];
     this.userDetails.watchlist = Boolean(this.userDetails[`watchlist`]);
-    this.userDetails.alreadyWatched = Boolean(this.userDetails[`already_watched`]);
-    this.userDetails.watchingDate = this.userDetails[`watching_date`] ? new Date(this.userDetails[`watching_date`]) : null;
+    this.userDetails.already_watched = Boolean(this.userDetails[`already_watched`]);
+    this.userDetails.watching_date = this.userDetails[`watching_date`] ? new Date(this.userDetails[`watching_date`]) : null;
     this.userDetails.favorite = Boolean(this.userDetails[`favorite`]);
 
     this.setComments = this.setComments.bind(this);
@@ -37,13 +38,32 @@ export default class Movie {
     return {
       'id': this.id,
 
-      'comments': Array.from(this.comments),
+      'comments': [],
 
-      'personal_rating': this.userDetails.personalRating,
-      'watchlist': this.userDetails.watchlist,
-      'already_watched': this.userDetails.alreadyWatched,
-      'watching_date': this.userDetails.watchingDate ? this.userDetails.watchingDate.toISOString() : null,
-      'favorite': this.userDetails.favorite,
+      'film_info': {
+        'title': this.filmInfo.title,
+        'alternative_title': this.filmInfo.alternative_title,
+        'total_rating': this.filmInfo.total_rating,
+        'poster': this.filmInfo.poster,
+        'age_rating': this.filmInfo.age_rating,
+        'director': this.filmInfo.director,
+        'actors': Array.from(this.filmInfo.actors),
+        'writers': Array.from(this.filmInfo.writers),
+        'release': {
+          'date': this.filmInfo.release.date,
+          'release_country': this.filmInfo.release.release_country,
+        },
+        'runtime': this.filmInfo.runtime,
+        'genre': Array.from(this.filmInfo.genre),
+        'description': this.filmInfo.description,
+      },
+      'user_details': {
+        'personal_rating': this.userDetails.personal_rating,
+        'watchlist': this.userDetails.watchlist,
+        'already_watched': this.userDetails.already_watched,
+        'watching_date': this.userDetails.watching_date ? this.userDetails.watching_date.toISOString() : null,
+        'favorite': this.userDetails.favorite,
+      }
     };
   }
 
