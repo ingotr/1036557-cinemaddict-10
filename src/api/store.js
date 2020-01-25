108d2af76b421b049a7d74d83fb6847999b20d4e
Aleksandr <markovkin88@gmail.com>
2020-01-25 20:32:24 +0300

    api/store: Реазилует методы хранилища

--- src/api/store.js
+++ src/api/store.js
@@ -5,14 +5,34 @@ export default class Store {
   }
 
   getAll() {
-  }
-
-  dropAll() {
+    try {
+      return JSON.parse(this._storage.getItem(this._storeKey));
+    } catch (err) {
+      return {};
+    }
   }
 
   setItem(key, value) {
+    const store = this.getAll();
+
+    this._storage.setItem(
+        this._storeKey,
+        JSON.stringify(
+            Object.assign({}, store, {[key]: value})
+        )
+    );
   }
 
   removeItem(key) {
+    const store = this.getAll();
+
+    delete store[key];
+
+    this._storage.setItem(
+        this._storeKey,
+        JSON.stringify(
+            Object.assign({}, store)
+        )
+    );
   }
 }
