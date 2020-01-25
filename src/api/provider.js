8771c6451fa8c9181d1ae3f7ce65275a174636d6
Aleksandr <markovkin88@gmail.com>
2020-01-25 21:08:36 +0300

    api/provider: Добавляет провайдеру функционал записи данных в хранилище

--- src/api/provider.js
+++ src/api/provider.js
@@ -9,16 +9,30 @@ export default class Provider {
 
   getMovies() {
     if (this._isOnline()) {
-      return this._api.getMovies();
+      return this._api.getMovies()
+        .then((movies) => {
+          movies.forEach((movie) => this._store.setItem(movie.id. movie.toRaw()));
+          return movies;
+        });
     }
 
-    return Promise.resolve(Movie.parseMovies([]));
+    const storeMovies = Object.values(this._store.getAll());
+
+    return Promise.resolve(Movie.parseMovies(storeMovies));
   }
 
   updateMovie(movieId, data) {
     if (this._isOnline()) {
-      return this._api.updateMovie(movieId, data);
+      return this._api.updateMovie(movieId, data)
+      .then((newMovie) => {
+        this._store.setItem(newMovie.movieId, newMovie.toRa());
+        return newMovie;
+      });
     }
+
+    const fakeUpdatedMovie = Movie.parseMovie(Object.assign({}, data.toRaw(), {movieId}));
+
+    this._store.setItem(movieId, Object.assign({}, fakeUpdatedMovie.toRaw(), {offline: true}));
 
     return Promise.resolve(data);
   }
@@ -29,27 +43,46 @@ export default class Provider {
 
   getComments(movieId) {
     if (this._isOnline()) {
-      return this._api.getComments(movieId);
+      return this._api.getComments(movieId)
+      .then((comments) => {
+        comments.forEach((comment) => {
+          this._store.setItem(comment.id, comment.toRAW());
+        });
+        return comments;
+      });
     }
 
-    return Promise.resolve(Comment.parseComments([]));
+    const storeComments = Object.values(this._store.getAll());
+
+    return Promise.resolve(Comment.parseComments(storeComments));
   }
 
   createComment(movieId, data) {
     if (this._isOnline()) {
-      return this._api.createComment(movieId, data);
+      return this._api.createComment(movieId, data)
+      .then((newComment) => {
+        this._store.setItem(newComment, newComment.toRAW());
+        return newComment;
+      });
     }
 
     const fakeNewCommentId = new Date().toDateString();
-    const fakeNewComment = Comment.parseComment(Object.assign({}, data.toRaw(), {id: fakeNewCommentId}));
+    const fakeNewComment = Comment.parseComment(Object.assign({}, data.toRAW(), {id: fakeNewCommentId}));
+
+    this._store.setItem(fakeNewComment.id, Object.assign({}, fakeNewComment.toRAW(), {offline: true}));
 
     return Promise.resolve(fakeNewComment);
   }
 
   deleteComment(commentId) {
     if (this._isOnline()) {
-      return this._api.deleteComment(commentId);
+      return this._api.deleteComment(commentId)
+      .then(() => {
+        this._store.removeItem(commentId);
+      });
     }
+
+    this._store.removeItem(commentId);
 
     return Promise.resolve();
   }
