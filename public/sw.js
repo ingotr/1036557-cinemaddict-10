19c82c367e9bac1d71c8037e1ad1996eb588a379
Aleksandr <markovkin88@gmail.com>
2020-01-25 18:53:08 +0300

    public/sw: Кэширует файлы при регистрации ServiceWorker

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,31 @@
+const CACHE_PREFIX = `cinemaddict-cache`;
+const CACHE_VER = `v1`;
+const CACHE_NAME = `${CACHE_PREFIX}-${CACHE_VER}`;
+
 self.addEventListener(`install`, (evt) => {
+  evt.waitUntil(
+      caches.open(CACHE_NAME)
+        .then((cache) => {
+          return cache.addAll([
+            `/`,
+            `/index.html`,
+            `/bundle.js`,
+            `/css/normalize.css`,
+            `/css/main.css`,
+            `/images/emoji/angry.png`,
+            `/images/emoji/puke.png`,
+            `images/emoji/sleeping.png`,
+            `/images/emoji/smile.png`,
+            `/images/emoji/trophy.png`,
+            `/imges/icons/icon-favorite-active.svg`,
+            `/imges/icons/icon-favorite.svg`,
+            `/imges/icons/icon-watched-active.svg`,
+            `/imges/icons/icon-watched.svg`,
+            `/imges/icons/icon-watchlist-active.svg`,
+            `/imges/icons/icon-watchlist.svg`,
+          ]);
+        })
+  );
 });
 
 self.addEventListener(`activate`, (evt) => {
